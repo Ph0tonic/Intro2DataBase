3009b03154488602349dba3ed1eb86a1d09f9974
Bastien Wermeille <bastien.wermeille@gmail.com>
2020-02-26 10:16:07 +0100

    Ajout schema v1

---
+++ parse.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+business_file = 'yelp_academic_dataset_business.csv'
+review_file = 'yelp_academic_dataset_review.csv'
+tip_file = 'yelp_academic_dataset_tip.csv'
+user_file = 'yelp_academic_dataset_user.csv'
+
+# df = pd.read_csv('data_in.csv', names=['Label','Requirements'], skiprows=1) # This assumes and skips the header row ('TSD' in your question)
