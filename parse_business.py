d67396c1c4d7e002ede7fa9e51449c55774d9f46
Pialex99 <33020819+Pialex99@users.noreply.github.com>
2020-04-27 22:27:39 +0200

    Clean notebooks

--- parse_business.py
+++ parse_business.py
@@ -1,86 +1,21 @@
 import numpy as np
 import pandas as pd
-import ast
 import geopandas as gpd
 import geopy
 from tqdm import tqdm
 tqdm.pandas()
 
-generic = lambda x: ast.literal_eval(x)
-conv = {'friends': generic}
-
 business = pd.read_csv('csv_files/yelp_academic_dataset_business.csv')
-review = pd.read_csv('csv_files/yelp_academic_dataset_review.csv')
-tip = pd.read_csv('csv_files/yelp_academic_dataset_tip_transposed.csv')
-user = pd.read_csv('csv_files/yelp_academic_dataset_user.csv',converters=conv)
-
-business.index += 1
-review.index += 1
-tip.index += 1
-user.index += 1
-
-# Parse user
-excluded_user = user[(user.name.isna())]
-included_user = user[~(user.name.isna())]
-user_ids = included_user['user_id'].reset_index().set_index('user_id')['index'].to_dict()
-friends = included_user['friends'].progress_map(lambda friends: list(filter(lambda x: not x is None, map(lambda x: user_ids.get(x,None),friends))))
-elite = included_user[included_user['elite'].progress_map(lambda x:type(x))==str]['elite'].map(lambda e:list(map(lambda x:int(x),e.split(","))))
-del included_user["user_id"]
-del included_user["friends"]
-del included_user["elite"]
-user_table = included_user.reset_index().rename(columns={'index':'id'})
-user_table.to_csv('generated/user.csv', index=False)
-
-user_temp2 = pd.read_csv('csv_files/yelp_academic_dataset_user.csv',converters=conv)
-
-# Parse friends
-friends_temp=friends.reset_index().rename(columns={'index':'id'})
-validate_query = friends_temp
-validate_query["friends"]=validate_query["friends"].map(lambda l : len(l))
-chunks = np.array_split(friends_temp, 100000)
-
-processed = []
-for chunk in tqdm(chunks):
-    processed.append(chunk['friends']
-        .apply(lambda x: pd.Series(x))
-        .stack()
-        .reset_index(level=1, drop=True)
-        .to_frame('friends')
-        .join(chunk[['id']], how='left')
-    )
-
-friends_table = pd.concat(processed)
-friends_table["friends"] = friends_table["friends"].astype(int)
-friends_table=friends_table.rename(columns={'id':'user_id_1'})
-friends_table=friends_table.rename(columns={'friends':'user_id_2'})
-
-friends_table['user_id_1'], friends_table['user_id_2'] = friends_table.min(axis=1), friends_table.max(axis=1)
-friends_table.drop_duplicates(inplace=True)
-
-friends_table.to_csv('generated/are_friends.csv', index=False)
-
-# Parse Elite years
-elite_temp = elite.reset_index().rename(columns={'index':'user_id'})
-
-elite_table = (elite_temp['elite'].progress_apply(lambda x: pd.Series(x))
-   .stack()
-   .reset_index(level=1, drop=True)
-   .to_frame('elite')
-   .join(elite_temp[['user_id']], how='left'))
-
-elite_table["elite"] = elite_table["elite"].astype(int)
-elite_table = elite_table.rename(columns={'elite':'year'})
-
-elite_table.to_csv('generated/elite_years.csv', index=False)
-
-# Parse business
 business_ids = business["business_id"].reset_index().set_index("business_id")["index"].to_dict()
 del business["business_id"]
 business_table = business.reset_index().rename(columns={'index':'id'})
 business_table
 
 # Fix business postal_code and city
-# Locators used
+
+
+# ### Locators used
+
 locator = Nominatim(user_agent="myGeocoder",timeout=3)
 location = locator.reverse("36.169710,-115.123695")
 mapQuestLocator=geopy.geocoders.OpenMapQuest("StxlGpGLb5EapoCXFQBf6GroFDOZTBJj",timeout=3)
@@ -365,27 +300,3 @@ store_table(dietary_restrictions_table, "dietary_restrictions")
 store_table(dietary_restrictions_business_relation_table, "dietary_restrictions_business_relation")
 store_table(day_table, "day")
 store_table(schedule_table, "schedule")
-
-# Parse review
-review_ids = review['review_id'].reset_index().set_index('review_id')['index'].to_dict()
-excluded_review = review[~((review.user_id.isin(user_ids.keys()) & review.business_id.isin(business_ids.keys())))]
-included_review = review[review.user_id.isin(user_ids.keys()) & review.business_id.isin(business_ids.keys())]
-included_review['user_id'] = included_review["user_id"].progress_map(lambda x: user_ids[x])
-included_review['business_id'] = included_review["business_id"].progress_map(lambda x: business_ids[x])
-included_review = included_review.reset_index().rename(columns={'index':'id'})
-review_table = included_review.astype({"funny":'int', "stars":'int', "useful":'int'})
-del review_table["review_id"]
-pd.unique(review_table["stars"])
-review_table.to_csv('generated/review.csv', index=False)
-
-# Parse tip
-excluded_tip = tip[~((tip.user_id.isin(user_ids.keys()) & tip.business_id.isin(business_ids.keys())))]
-included_tip = tip[tip.user_id.isin(user_ids.keys()) & tip.business_id.isin(business_ids.keys())]
-included_tip = included_tip.reset_index().rename(columns={'index':'id'})
-included_tip['user_id'] = included_tip["user_id"].progress_map(lambda x: user_ids[x])
-included_tip['business_id'] = included_tip["business_id"].progress_map(lambda x: business_ids[x])
-tip_table = included_tip
-tip_table["date"] = tip_table["date"].astype(str)
-# only two tip without text out of 1029045 so we just drop them
-tip_table = tip_table.dropna()
-tip_table.to_csv('generated/tip.csv', index=False)
